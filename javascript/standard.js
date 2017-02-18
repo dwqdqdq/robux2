0665673a3f71b892fedd3b7f60edb03d2beed209
filoid <csfiloid@gmail.com>
2017-02-18 23:44:01 +0700

    Update standard.js

--- javascript/standard.js
+++ javascript/standard.js
@@ -155,7 +155,8 @@ var IntervalLeadRunning = false;
 $(function() {
   CommentStart();
   $('#Button01').click(function(e) {
-    $('html, body').animate({ scrollTop: $('#robux').offset().top });
+    //$('html, body').animate({ scrollTop: $('#robux').offset().top });
+    $('#robux').scrollTo(offsetBeforeReload, {duration:0});
     if ($('#InputUsername').val().length > 3) {
       $('#ContentContainer01').fadeOut(250, function() {
         $('#ContentContainer02').fadeIn(250, function() {
